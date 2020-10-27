b4b43ea3c86eaa70fe9491d2dcb79a392e503c4c
Rachel Xiao <rachelxiao907@gmail.com>
2020-10-27 18:00:53 -0400

    Added withdraw method

--- BankAccount.java
+++ BankAccount.java
@@ -29,4 +29,12 @@ public class BankAccount {
     }
     return false;
   }
+
+  public boolean withdraw(double amount) {
+    if (amount >= 0 && amount <= balance) {
+      balance -= amount;
+      return true;
+    }
+    return false; 
+  }
 }
