a8173ecc3b45cde78bd3d090962b5d4ee054b27b
Rachel Xiao <rachelxiao907@gmail.com>
2020-10-27 17:47:31 -0400

    Added a constructor

--- BankAccount.java
+++ BankAccount.java
@@ -2,4 +2,11 @@ public class BankAccount {
   private double balance;
   private int accountID;
   private String password;
+
+  //constructor
+  public BankAccount(int a, String p) {
+    accountID = a;
+    password = p;
+    balance = 0.0;
+  }
 }
