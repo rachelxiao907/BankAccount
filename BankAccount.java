808480701f38648940915ec5757c3bede3cfd101
Rachel Xiao <rachelxiao907@gmail.com>
2020-10-27 17:56:45 -0400

    Added deposit method

--- BankAccount.java
+++ BankAccount.java
@@ -21,4 +21,12 @@ public class BankAccount {
   public void setPassword(String pw) {
     password = pw;
   }
+
+  public boolean deposit(double amount) {
+    if (amount >= 0) {
+      balance += amount;
+      return true;
+    }
+    return false;
+  }
 }
