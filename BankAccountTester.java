8911f249ea885d8b7b4b96c6adcac5db01e81652
Rachel Xiao <rachelxiao907@gmail.com>
2020-10-31 18:42:28 -0400

    Added new test cases for CW

--- BankAccountTester.java
+++ BankAccountTester.java
@@ -19,5 +19,14 @@ public class BankAccountTester {
     //obj.method could be static or non static
     BankAccount b2 = new BankAccount(5213452, "no");
     System.out.println(b1.transferTo(b2, 1000, "hello"));
+
+    BankAccount t1 = new BankAccount(8234, "yup");
+    BankAccount t2 = new BankAccount(3284, "dog");
+    t1.deposit(50);
+    System.out.println(t1.getBalance());
+    System.out.println(t2.getBalance());
+    System.out.println(t1.transferTo(t2, 30, "yup"));
+    System.out.println(t2.getBalance());
+    System.out.println(t1.getBalance());
   }
 }
